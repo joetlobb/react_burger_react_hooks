d0637f12665555be0417df1a440e302bc09127a9
Joetlobb <joetlobb@Joetlobbs-MacBook-Pro.local>
2020-12-25 13:59:33 +0700

    Converting <App>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import asyncComponent from './hoc/asyncComponent/asyncComponent';
@@ -18,41 +18,39 @@ const AsyncAuth = asyncComponent(() => {
   return import('./containers/Auth/Auth');
 });
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onAutoLogin();
-  }
+const App = props => {
+  useEffect(() => {
+    props.onAutoLogin();
+    console.log('AUTO LOGIN RUNNING');
+  }, [props]);
 
-  render() {
-    let routes = (
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={AsyncAuth} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes =
       <Switch>
+        <Route path="/checkout" component={AsyncCheckout} />
+        <Route path="/orders" component={AsyncOrders} />
+        <Route path="/logout" component={Logout} />
         <Route path="/auth" component={AsyncAuth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
-    );
-
-    if (this.props.isAuthenticated) {
-      routes =
-        <Switch>
-          <Route path="/checkout" component={AsyncCheckout} />
-          <Route path="/orders" component={AsyncOrders} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={AsyncAuth} />
-          <Route path="/" exact component={BurgerBuilder} />
-          <Redirect to="/" />
-        </Switch>
-    }
-
-    return (
-      <div >
-        <Layout>
-          {routes}
-        </Layout>
-      </div >
-    );
   }
-}
+
+  return (
+    <div>
+      <Layout>{routes}</Layout>
+    </div>
+  );
+};
+
 const mapStateToProps = state => {
   return {
     isAuthenticated: state.auth.token !== null
@@ -64,4 +62,5 @@ const mapDispatchToProps = dispatch => {
     onAutoLogin: () => dispatch(actions.authCheckState())
   }
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(App);
